47330516a5097d3474e6a78e25e26f35a1269c76
Pedro Furtado <pedro.nazare@pdcase.com.br>
2023-03-25 09:59:12 -0300

    feat: alias para entidade

--- cruddemo/src/main/java/com/ppfurtado/cruddemo/repository/EmployeeRepository.java
+++ cruddemo/src/main/java/com/ppfurtado/cruddemo/repository/EmployeeRepository.java
@@ -3,9 +3,9 @@ package com.ppfurtado.cruddemo.repository;
 
 import com.ppfurtado.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
-@Repository
+@RepositoryRestResource(path = "empregados")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
